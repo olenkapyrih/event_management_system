03f8febf6e44e48d4b7296f68144b4e75156e8f1
olenkapyrih <olena.pyrih.kn.2021@lpnu.ua>
2023-07-19 17:10:39 +0300

    configuration file with fields validation added

---
+++ config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DEBUG: bool
+    SECRET_KEY: str
+
+    HOST: str
+    PORT: int
+
+    CORS_ALLOW_ALL_ORIGINS: bool
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
